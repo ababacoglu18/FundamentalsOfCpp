4284856aea8ed743a9444301bb8678aaf8289ef3
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-01-29 03:52:11 +0300

    OOP

---
+++ Merbekta/17ConstructorsDestructors.cpp
@@ -0,0 +1,43 @@
+/*#include <iostream>
+
+
+
+
+class lamba {
+public:
+
+
+	bool durum;
+
+	lamba(bool _durum) {
+		durum = _durum;      //constructor.
+		durumGoster();
+
+	}
+
+
+	void durumGoster(){
+
+		std::cout << durum << std::endl;
+
+	  }
+
+		
+
+	  ~lamba() {
+
+		  std::cout << "nesne silindi \t";       //destructor.
+
+
+	}
+
+};
+int main() {
+
+	lamba lamba1(true);
+	lamba1.durumGoster();
+
+
+
+
+}*/
---
+++ Merbekta/18StaticMembers.cpp
@@ -0,0 +1,38 @@
+/*#include <iostream>
+
+class Player {
+public:
+
+	static int OyuncuSayisi;
+
+	Player() {
+
+		OyuncuSayisi++;
+	}
+
+	static int sayi() {
+
+		return OyuncuSayisi;
+	}
+
+
+
+};
+int Player::OyuncuSayisi = 0;
+
+
+
+
+int main() {
+
+	Player oyuncu1;	
+	Player oyuncu2;
+
+	std::cout<<Player::OyuncuSayisi<<std::endl;  
+
+	std::cout << Player::sayi()<<std::endl; //sýnýf üzerinden eriþim
+
+	std::cout << oyuncu1.sayi();            //Nesne üzerinden eriþim
+
+											//Log dosyalarý
+}*/
