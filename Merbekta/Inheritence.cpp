78885f58f1e96162a08f919b605a403bd78c02a5
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-01-29 04:25:38 +0300

    Inheritence

---
+++ Merbekta/Inheritence.cpp
@@ -0,0 +1,47 @@
+/*#include<iostream>
+
+
+
+		class Actor {
+		public:
+			int boyut;
+			int lokasyon;
+
+			void hareketEt() {
+
+
+
+			}
+		};
+
+
+		class Light : public Actor {
+		public:
+			void Hareket() {
+
+				hareketEt();
+
+			}
+
+
+};
+
+		class Mesh : public Light {
+		public:
+			void IsigiHareketEttir() {
+
+				Hareket();
+			}
+
+		};
+
+
+int main(void) {
+
+	Light isik1;
+
+
+											
+
+
+}*/
